ba288deff1246782cae63dee93036e9a285fdbeb
xuanxwz <1215007121@qq.com>
2019-07-29 17:42:29 +0800

    dai l daily
    ;

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <a href='#'>github: </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
---
+++ src/views/Details.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <el-page-header @back="goBack" content="详情页面"></el-page-header>
+
+    <div class="container">
+<file-pond
+        name="test"
+        ref="pond"
+        label-idle="Drop files here..."
+        allow-multiple="true"
+        accepted-file-types="image/jpeg, image/png"
+        server="/api"
+        v-bind:files="myFiles"
+        v-on:init="handleFilePondInit"
+        class="file-pond"
+      />
+    </div>
+  </div>
+</template>
+
+
+<script>
+ // Import Vue FilePond
+import vueFilePond from 'vue-filepond';
+
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css';
+
+// Import FilePond plugins
+// Please note that you need to install these plugins separately
+
+// Import image preview plugin styles
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+
+// Import image preview and file type validation plugins
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+
+// Create component
+const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview);
+export default {
+
+  name: 'details',
+   data: function() {
+        return { myFiles: ['cat.jpeg'] };
+    },
+    methods: {
+        handleFilePondInit: function() {
+            console.log('FilePond has initialized');
+
+            // FilePond instance methods are available on `this.$refs.pond`
+        }
+    },
+
+  methods: {
+    goBack() {
+      console.log("go back");
+    }
+  },
+  components: {
+        FilePond
+    }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 700px;
+  background: gray;
+}
+
+.file-pond {
+  margin: 20px;
+  width: 300px;
+  height: 50px;
+}
+</style>
+
