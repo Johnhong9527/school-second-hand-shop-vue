f7f0886f0b55b54f6a01ae5716601780d8fb699e
xuanxwz <1215007121@qq.com>
2019-07-30 09:15:31 +0800

    fix

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -3,7 +3,7 @@
     <el-page-header @back="goBack" content="详情页面"></el-page-header>
 
     <div class="container">
-<file-pond
+      <file-pond
         name="test"
         ref="pond"
         label-idle="Drop files here..."
@@ -15,42 +15,46 @@
         class="file-pond"
       />
     </div>
+
+    <el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop>
   </div>
 </template>
 
 
 <script>
- // Import Vue FilePond
-import vueFilePond from 'vue-filepond';
+// Import Vue FilePond
+import vueFilePond from "vue-filepond";
 
 // Import FilePond styles
-import 'filepond/dist/filepond.min.css';
+import "filepond/dist/filepond.min.css";
 
 // Import FilePond plugins
 // Please note that you need to install these plugins separately
 
 // Import image preview plugin styles
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
 
 // Import image preview and file type validation plugins
-import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 
 // Create component
-const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview);
+const FilePond = vueFilePond(
+  FilePondPluginFileValidateType,
+  FilePondPluginImagePreview
+);
 export default {
+  name: "details",
+  data: function() {
+    return { myFiles: ["cat.jpeg"] };
+  },
+  methods: {
+    handleFilePondInit: function() {
+      console.log("FilePond has initialized");
 
-  name: 'details',
-   data: function() {
-        return { myFiles: ['cat.jpeg'] };
-    },
-    methods: {
-        handleFilePondInit: function() {
-            console.log('FilePond has initialized');
-
-            // FilePond instance methods are available on `this.$refs.pond`
-        }
-    },
+      // FilePond instance methods are available on `this.$refs.pond`
+    }
+  },
 
   methods: {
     goBack() {
@@ -58,15 +62,15 @@ export default {
     }
   },
   components: {
-        FilePond
-    }
+    FilePond
+  }
 };
 </script>
 
 <style scoped>
 .container {
   width: 100%;
-  height: 700px;
+  height: 1200px;
   background: gray;
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/img/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <CommidtyCard />
+    </el-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import CommidtyCard from "@/components/CommidtyCard.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    CommidtyCard
   }
-}
+};
 </script>
